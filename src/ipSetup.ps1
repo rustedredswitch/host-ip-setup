85e332f6849028e87ae9fe41e9ad8c15fefdd562
Alessandro Rosa <alessandrorosa770@gmail.com>
2024-05-10 12:58:29 +0200

    reversing the order of the hosts file update

--- src/ipSetup.ps1
+++ src/ipSetup.ps1
@@ -25,6 +25,10 @@ $Ip = $NetConnection.SourceAddress.IPAddress
 Write-Output "Current IP: $Ip"
 Write-Output "Current interface: $InterfaceAlias"
 
+# Update Linux hosts file
+Invoke-Expression -Command "wsl chmod +x $linuxBashScript"
+Invoke-Expression -Command "wsl sudo bash $linuxBashScript $linuxHostsPath $hostname $Ip"
+
 # Update Windows hosts file
 $windowsHostsContent = Get-Content -Path $windowsHostsPath
 $windowsLine = $windowsHostsContent | Where-Object { $_ -match $hostname }
@@ -32,10 +36,6 @@ $IpWin = ($windowsLine -split '\s+')[0]
 Write-Output "Old $hostname IP in Windows: $IpWin"
 $windowsHostsContent = $windowsHostsContent -replace "$IpWin", $Ip
 [System.IO.File]::WriteAllLines($windowsHostsPath, $windowsHostsContent)
-
-# Update Linux hosts file
-Invoke-Expression -Command "wsl chmod +x $linuxBashScript"
-Invoke-Expression -Command "wsl sudo bash $linuxBashScript $linuxHostsPath $hostname $Ip"
 
 Write-Output "IP configuration setup completed"
 Write-Output "Have a nice day"
